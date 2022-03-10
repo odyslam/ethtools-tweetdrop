4546d6e5c28388a23efe7490a17c4c95d8db06f1
odyslam.eth <odyslam@gmail.com>
2022-03-10 07:59:32 +0200

    Feat/tweetdrop (#3)

--- src/handler.ts
+++ src/handler.ts
@@ -1,24 +1,57 @@
-import Scraper from "../tweetdrop/src/scraper"; // Scraper
-import { logger } from "./tweetdrop/src/logger"; // Logging
+import Scraper from "./scraper"; // Scraper
 
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
-}
-
-async function scrapeWorker(conversationId, twitterBearer, numTokens, rpcProvider){
-  // If no conversation id or twitter token provided
-  if (!conversationID || !twitterBearer) {
-    // Throw error and exit
-    logger.error("Missing required parameters, update .env");
-    process.exit(1);
+  const { pathname } = new URL(request.url);
+  if (pathname.startsWith("/scrape")) {
+    let tokens = pathname.split('/');
+    let thread = tokens[2];
+    let tokensToGive = parseInt(tokens[3]);
+    console.log("Thread: ", thread);
+    console.log("Tokens to give: ", tokensToGive);
+    console.log("rpc url: ", ETH_RPC_URL);
+    console.log("twitter api token: ", TWITTER_BEARER_TOKEN);
+    return await scrapeWorker(thread, tokensToGive);
   }
-
-  // Scrape tweets for addresses
-  const scraper = new Scraper(
-    conversationID,
-    twitterBearer,
-    numTokens,
-    rpcProvider
-  );
-  await scraper.scrape();
+  return new Response("<html><p>Visit <b>/scrape/twitter_thread_id/tokens_per_address</b> and you will get a list you can copy to Disperse or Multisender</p></html>", {
+      headers: {
+        "content-type": "text/html;charset=UTF-8"
+      },
+      status: 404
+    })
 }
+
+  async function scrapeWorker(conversationId: string, tokensToGive: number): Promise<Response>{
+    let rpcProvider = ETH_RPC_URL || "";
+    let twitterBearer = TWITTER_BEARER_TOKEN || "";
+    // If no conversation id or twitter token provided
+    let json;
+    if (!conversationId) {
+      json = {"status": "error", "output": "no conversation id"};
+    }
+    else {
+      // Scrape tweets for addresses
+      const scraper = new Scraper(
+        conversationId,
+        twitterBearer,
+        tokensToGive,
+        rpcProvider
+      );
+      let addresses = await scraper.scrape();
+      let output: string = "";
+      addresses.forEach((address)=> {
+        output = output  + "<p>" + address + "</p>";
+      });
+      "<html>"+ output + "</html>"
+      return new Response(output, {
+        headers: {
+          "content-type": "text/html;charset=UTF-8"
+        }
+      });
+    }
+    json = JSON.stringify(json, null, 2);
+    return new Response(json, {
+      headers: {
+        "content-type": "application/json;charset=UTF-8"
+      }
+    })
+  }
